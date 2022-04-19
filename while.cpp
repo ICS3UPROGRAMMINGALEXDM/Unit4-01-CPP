c5abba9bae5f214f39830ebca7a0dbd914a0f5e7
Ubuntu <ubuntu@ip-172-31-17-170.ec2.internal>
2022-04-19 22:25:50 +0000

    reformatted

--- while.cpp
+++ while.cpp
@@ -13,29 +13,29 @@ using std::string;
 
 int main() {
     // Initializing variables
-    string u_num;
-    int loop_num = 0, loop_sum = 0, numInt;
+    string uNum;
+    int loopNum = 0, loopSum = 0, numInt;
 
     cout << "Enter a positive number: ";
-    cin >> u_num;
+    cin >> uNum;
 
     try {
         // converting to int
-        numInt = stoi(u_num);
+        numInt = stoi(uNum);
 
         // only execute if condition is met
         if (numInt > 0) {
             // loop is used to go through each number and add to sum
-            while (loop_num < numInt) {
+            while (loopNum < numInt) {
                 // adds one to the loop counter
-                loop_num += 1;
-                cout << loop_num << " times through loop." << endl;
+                loopNum += 1;
+                cout << loopNum << " times through loop." << endl;
                 // update sum
-                loop_sum += loop_num;
+                loopSum += loopNum;
             }
 
             cout << "The sum of the numbers between 0-" << numInt <<
-            " is " << loop_sum;
+            " is " << loopSum;
 
         } else {
             cout << "Invalid Input";
